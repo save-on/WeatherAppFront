02ebcdedc644ba82ce1963a92225e6209494ec65
MrSurge87 <Mendozsa.sm@gmail.com>
2025-01-13 18:01:12 -0500

    added simple footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div>Developed By Sayvon & Sergio</div>
+            <div>2025</div>
+        </footer>
+    );
+};
+
+export default Footer;
