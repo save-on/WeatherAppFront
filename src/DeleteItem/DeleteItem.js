16c33a2a405a170aa9ed9ddbab4fd34fe3d79d36
MrSurge87 <Mendozsa.sm@gmail.com>
2025-01-12 22:19:11 -0500

    added delete item modal feature

---
+++ src/DeleteItem/DeleteItem.js
@@ -0,0 +1,42 @@
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import React from "react";
+import close from "../../images/close.svg";
+import "./DeleteItem.css";
+
+const DeleteItemModal = ({ onClose, onDeleteClick }) => {
+  return (
+    <div className={"modal delte"}>
+      <div className="delete__modal-container">
+        <div className="delete__modal">
+          <button className="delete__modal-close">
+            <img
+              className="delete-button"
+              src={close}
+              onClick={onClose}
+              alt="Close Button"
+            />
+          </button>
+          <div className="delete__modal-content">
+            <p className="delete__modal-text">
+              Are you sure you want to delete this item?
+              <br></br>
+              This action is irreversible!
+            </p>
+          </div>
+        </div>
+        <div className="delete__modal-button">
+          <button className="delete__button-confirm" onClick={onDeleteClick}>
+            Yes, delete item
+          </button>
+        </div>
+        <div className="delete__modal-button">
+          <button className="delete__button-cancel" onClick={onClose}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteItemModal;
