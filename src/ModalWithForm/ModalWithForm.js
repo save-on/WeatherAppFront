7e1ccaf88461ffd14873b3eac5566271b5f44d8d
MrSurge87 <Mendozsa.sm@gmail.com>
2025-01-13 21:20:21 -0500

    added ModalWithForm component and ModalWithForm css

---
+++ src/ModalWithForm/ModalWithForm.js
@@ -0,0 +1,28 @@
+import "./ModalWithForm.css";
+
+const ModalWithForm = ({
+  children,
+  buttonText = "",
+  title,
+  onClose,
+  name,
+  handleSubmit,
+  isOpen,
+  onSubmit,
+}) => {
+  return (
+    <div className={`modal modal_type_${name}`}>
+      <div className="modal__content">
+        <h3 className="add__garment-header">
+          {title}
+          <button className="close" type="button" onClick={onClose}></button>
+        </h3>
+        <form className="modal-form" onSubmit={onSubmit}>
+          {children}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ModalWithForm;
