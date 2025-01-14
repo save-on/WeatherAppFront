2a8ec396a414593aace6ca27062c951cf5174723
MrSurge87 <Mendozsa.sm@gmail.com>
2025-01-13 21:53:48 -0500

    added profile and sidebar components and css

---
+++ src/Profile/Profile.js
@@ -0,0 +1,43 @@
+import React from "react";
+import SideBar from "./SideBard";
+import ClothesSection from "./ClothingSection";
+import "./Profile.css";
+
+const Profile = ({
+  onCreate,
+  clothingItems,
+  onSelectCard,
+  onAddItem,
+  onCardLike,
+  onEditProfile,
+  onSignOut,
+  onDeleteClick,
+}) => {
+  return (
+    <div className="profile">
+      <SideBar onSignout={onSignOut} onEditProfile={onEditProfile} />
+      <div className="profile__items-container">
+        <div className="profile__items-text">
+          Your Clothing Items
+          <button
+            className="profile__add-button"
+            onClick={onCreate}
+            type="button"
+          >
+            + Add New
+          </button>
+        </div>
+        <ClothingSection
+          clothingItems={clothingItems}
+          onSelectCard={onSelectCard}
+          onCreate={onCreate}
+          onAddItem={onAddItem}
+          onCardLike={onCardLike}
+          onDeleteClick={onDeleteClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
