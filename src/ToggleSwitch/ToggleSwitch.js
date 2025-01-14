fbf08d631eb4fa1416b0991ec8e8a4784fd47d33
MrSurge87 <Mendozsa.sm@gmail.com>
2025-01-13 22:25:23 -0500

    added toggle switch component and css

---
+++ src/ToggleSwitch/ToggleSwitch.js
@@ -0,0 +1,42 @@
+import React, { useState, useContext } from "react";
+import "./ToggleSwitch.css";
+import { CurrentTemperatureUnitContex } from "../../contexts/CurrentTemperatureUnitContex";
+
+const ToggleSwitch = () => {
+  const { currentTemperatureUnit, handleToggleSwitchChange } = useContext(
+    CurrentTemperatureUnitContex
+  );
+  return (
+    <label className="switch" htmlFor="temperature-switch">
+      <input
+        className="switch__box"
+        id="temperature-switch"
+        type="checkbox"
+        onChange={handleToggleSwitchChange}
+      />
+      <span
+        className={
+          currentTemperatureUnit === "F"
+            ? "switch__slider switch__slider-C"
+            : "switch__slider switch__slide-F"
+        }
+      ></span>
+      <p
+        className={`switch__temp-F ${
+          currentTemperatureUnit === "F" && "swtich__active-F"
+        }`}
+      >
+        F
+      </p>
+      <p
+        className={`switch__temp-C ${
+          currentTemperatureUnit === "C" && "swtich__active-C"
+        }`}
+      >
+        C
+      </p>
+    </label>
+  );
+};
+
+export default ToggleSwitch;
