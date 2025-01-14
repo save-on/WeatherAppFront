dc07d694f0e14648e76ed22f239aedc0b89f9d9a
MrSurge87 <Mendozsa.sm@gmail.com>
2025-01-13 20:56:17 -0500

    added ItemModal component and ItemModal css

---
+++ src/ItemModal/ItemModal.js
@@ -0,0 +1,36 @@
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import "./ItemModal.css";
+import React, { useContext } from "react";
+
+const ItemModal = ({ selectedCard, onClose, onDeleteClick, loggedIn }) => {
+  return (
+    <div className={"modal"}>
+      <div className="preview-image-content">
+        <button
+          className="preview-image-close"
+          type="button"
+          onClick={onClose}
+        ></button>
+        <img
+          className="image-preview"
+          src={selectedCard.imageUrl}
+          alt="image-preview"
+        ></img>
+        <div className="preview-image-name">
+          {selectedCard.name}
+          {loggedIn && (
+            <button className="delete-button" onClick={onDeleteClick}>
+              Delete Item
+            </button>
+          )}
+        </div>
+        <div className="preview-image-weather-type">
+          {" "}
+          Weather Type: {selectedCard.weather}{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ItemModal;
