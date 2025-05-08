7a6fa0e9a85b1014c3c3f1e9d7175d625f485f63
MrSurge87 <Mendozsa.sm@gmail.com>
2025-05-07 21:34:50 -0400

    updated date picker css and added selected dates, cancel button, and submit button

--- src/Components/DateRangePicker/DateRangePicker.js
+++ src/Components/DateRangePicker/DateRangePicker.js
@@ -2,7 +2,7 @@ import "./DateRangePicker.css";
 import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 
-function DateRangePicker({ onDateChange, onClose}) {
+function DateRangePicker({ onDateChange, onClose }) {
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [dateRange, setDateRange] = useState(null);
@@ -44,14 +44,13 @@ function DateRangePicker({ onDateChange, onClose}) {
 
   const formatDateDisplay = (date) => {
     if (date) {
-      const month = (date.getMonth() + 1).toString().padStart(2, '0');
-      const day = date.getDate().toString().padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, "0");
+      const day = date.getDate().toString().padStart(2, "0");
       const year = date.getFullYear();
       return `${month}/${day}/${year}`;
     }
     return "";
   };
-  
 
   return (
     <div className="dateRangePicker__container">
@@ -64,31 +63,51 @@ function DateRangePicker({ onDateChange, onClose}) {
         inline
         monthsShown={2}
         formatMonth={(date) => {
-          const options = {month: 'long'};
-          return new Intl.DateTimeFormat('en-US', options).format(date);
+          const options = { month: "long" };
+          return new Intl.DateTimeFormat("en-US", options).format(date);
         }}
       />
-     <div className="dateRangePicker__footer">
-      <div className="dateRangePicker__footer-left">
-        {startDate && endDate ? (
-          `${formatDateDisplay(startDate)} To ${formatDateDisplay(endDate)}`
-        ) : startDate ? (
-          `${formatDateDisplay(startDate)} To `
-        ) : (
-          "Select Dates"
-        )}
+      <div className="dateRangePicker__footer">
+        <div className="dateRangePicker__footer-left">
+          {startDate && endDate
+            ? (
+                <>
+                  <span className="selected-date-box">
+                    {formatDateDisplay(startDate)}
+                  </span>{" "}
+                  To{" "}
+                  <span className="selected-date-box">
+                    {formatDateDisplay(endDate)}
+                  </span>
+                </>
+            )
+            : startDate
+            ? (
+              <>
+              <span className="selected-date-box">{formatDateDisplay(startDate)}</span> To
+              </>
+            )
+            : (<>
+            <span className="selected-date-box">Select Dates</span></>)}
+        </div>
+        <div className="dateRangePicker__footer-right">
+          <button
+            type="button"
+            className="dateRangePicker__button dateRangePicker__button-cancel"
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
+          <button
+            type="button"
+            className="dateRangePicker__button dateRangePicker__button-submit"
+            onClick={handleSubmit}
+          >
+            Select Dates
+          </button>
+        </div>
       </div>
-      <div className="dateRangePicker__footer-right">
-        <button type="button" className="dateRangePicker__button dateRangePicker__button-cancel" onClick={handleCancel} >
-          Cancel
-        </button>
-        <button type="button" className="dateRangePicker__button dateRangePicker__button-submit" onClick={handleSubmit}>
-          Select Dates
-        </button>
-      </div>
-     </div>
     </div>
-    
   );
 }
 
