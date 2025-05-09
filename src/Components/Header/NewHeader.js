90b83202c34c8a8d51b454f9080af3a389e24364
MrSurge87 <Mendozsa.sm@gmail.com>
2025-05-08 22:15:48 -0400

    updated links in header css

--- src/Components/Header/NewHeader.js
+++ src/Components/Header/NewHeader.js
@@ -1,7 +1,9 @@
 import AvatarDefault from "../../Images/profile_default.svg";
 import "./NewHeader.css";
 import { Link } from "react-router";
+import { useContext } from "react";
 import HeaderDropbox from "../HeaderDropbox/HeaderDropbox.jsx";
+import CurrentUserContext from "../../Contexts/CurrentUserContext.js";
 
 function NewHeader({
   onRegister,
@@ -12,19 +14,23 @@ function NewHeader({
   handleOpenDropbox,
   onSignOut,
 }) {
+    const currentUser = useContext(CurrentUserContext);
+
   return (
     <div className="newHeader">
-      <p className="newHeader__title">Packly</p>
+        <Link to="/" className="newHeader__title-link">
+        <p className="newHeader__title">Packly</p>
+        </Link>
       <div className="newHeader__profile">
         {loggedIn ? (
           <div className="newHeader__profile">
-            <Link to="/profile">
+            <Link className="newHeader__profile-myTrips-link" to="/profile">
                 <p>My Trips</p>
                 </Link>
             <img
               className="newHeader__avatar_image"
-              src={AvatarDefault}
-              alt="avatar"
+              src={currentUser.avatar || AvatarDefault}
+              alt={`${currentUser.name}'s avatar`}
               onClick={handleOpenDropbox}
             />
             <HeaderDropbox
